97a55e41fb0101a19b573c1e14f2eb8f071840c5
Tyler Sustare <10850753+TylerSustare@users.noreply.github.com>
2022-12-22 15:46:36 -0800

    Create example react project

---
+++ packages/_example-react-ts/src/App.tsx
@@ -0,0 +1,12 @@
+import { MyCounter } from "@feedback-otter/react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <MyCounter />
+    </div>
+  );
+}
+
+export default App;
